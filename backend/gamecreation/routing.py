99ee7e562ded74280d2ba7ac9a7512afbcda5e10
Abhijeet <abhijeet.kumar@joshtechnologygroup.com>
2025-08-27 01:46:16 +0530

    poker-backend-01 Review Fixes:02

--- backend/gamecreation/routing.py
+++
@@ -1,8 +0,0 @@
-# gamecreation/routing.py
-from django.urls import re_path
-from gamecreation.consumers import PokerWebsocket
-
-websocket_urls = [
-    re_path(r"ws/poker/(?P<pokerid>\d+)/$", PokerWebsocket.as_asgi()),
-]
-
